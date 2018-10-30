aef4ad716ed89ec599b2dda260bfeb8808ef1534
jrfsj <jrfsj@cin.ufpe.br>
2018-10-30 11:17:54 -0300

    Add project files.

---
+++ KinectSkeletonRecorder/MainWindow.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Kinect;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+
+namespace KinectSkeletonRecorder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private MyKinect kinect;
+
+        private int records = 0;
+
+        private string _statusText = "Record";
+        public string StatusText
+        {
+            get
+            {
+                return _statusText;
+            }
+
+            set
+            {
+                if (_statusText != value)
+                {
+                    _statusText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private bool usingKinectStudio;
+
+        public MainWindow()
+        {
+            kinect = new MyKinect();
+            kinect.kinectSensor.IsAvailableChanged += KinectSensor_IsAvailableChanged;
+            DataContext = this;
+            InitializeComponent();
+            RecordButton.Content = "Record";
+        }
+
+        private void KinectSensor_IsAvailableChanged(object sender, IsAvailableChangedEventArgs e)
+        {
+            if (usingKinectStudio == false)
+            {
+                if (kinect.kinectSensor.IsAvailable == false)
+                {
+                    StatusText = "kinect not avaliable";
+                    RecordButton.IsEnabled = false;
+                }
+                else
+                {
+                    StatusText = "kinect avaliable";
+                    RecordButton.IsEnabled = true;
+                }
+            } else
+            {
+                StatusText = null;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // user clicked to start recording
+            if (kinect.IsRecording() == false)
+            {
+                if (TextBoxInput.Text == "")
+                {
+                    kinect.path = "playbacks/" + "recording_" + records.ToString() + ".txt";
+                } else
+                {
+                    kinect.path = "playbacks/" + TextBoxInput.Text + ".txt";
+                }
+                kinect.StartRecording();
+                StatusText = "Recording...";
+                RecordButton.Content = "Stop";
+                DisableControls();
+            } else
+            // user clicked to stop recording
+            {
+                kinect.StopRecording();
+                StatusText = "Recording saved as " +  kinect.path.ToString().Split('/')[1];
+                RecordButton.Content = "Record";
+                TextBoxInput.Text = null;
+                EnableControls();
+                records++;
+            }
+        }
+
+        private void DisableControls() { 
+            TextBoxInput.IsEnabled = false;
+            CheckBoxUsingKinectStudio.IsEnabled = false;
+            //ButtonTextBoxInput.IsEnabled = false;
+        }
+
+        private void EnableControls()
+        {
+            TextBoxInput.IsEnabled = true;
+            CheckBoxUsingKinectStudio.IsEnabled = true;
+            //ButtonTextBoxInput.IsEnabled = true;
+        }
+
+        /// <summary>
+        /// INotifyPropertyChangedPropertyChanged event to allow window controls to bind to changeable data
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            usingKinectStudio = true;
+            StatusText = null;
+            RecordButton.IsEnabled = true;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            usingKinectStudio = false;
+
+            if (kinect.kinectSensor.IsAvailable == false)
+            {
+                StatusText = "kinect not avaliable";
+                RecordButton.IsEnabled = false;
+            }
+            else
+            {
+                StatusText = "kinect avaliable";
+                RecordButton.IsEnabled = true;
+            }
+
+            RecordButton.IsEnabled = kinect.kinectSensor.IsAvailable;
+        }
+
+        private void FileName_TouchEnter(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            Debug.WriteLine("hit enter");
+        }
+    }
+
+
+}
---
+++ KinectSkeletonRecorder/MyKinect.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using System.Windows.Media;
+using System.Windows;
+using System.Diagnostics;
+using System.IO;
+
+namespace KinectSkeletonRecorder
+{
+    class MyKinect
+    {
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        public KinectSensor kinectSensor = null;
+
+        /// <summary>
+        /// Coordinate mapper to map one type of point to another
+        /// </summary>
+        private CoordinateMapper coordinateMapper = null;
+
+        /// <summary>
+        /// Reader for body frames
+        /// </summary>
+        private BodyFrameReader bodyFrameReader = null;
+
+        /// <summary>
+        /// Array for the bodies
+        /// </summary>
+        private Body[] bodies = null;
+
+        /// <summary>
+        /// Constant for clamping Z values of camera space points from being negative
+        /// </summary>
+        private const float InferredZPositionClamp = 0.1f;
+
+        public Dictionary<JointType, Joint> Joints { get; set; }
+
+        public Dictionary<JointType, Point> JointPoints { get; set; }
+
+        private bool _kinectIsReady = false;
+        public bool KinectIsReady
+        {
+            get
+            {
+                return _kinectIsReady;
+            }
+            set
+            {
+                if (_kinectIsReady != value)
+                {
+                    _kinectIsReady = value;
+                }
+            }
+        }
+
+        private bool record = false;
+        public string path = null;
+        private FileStream fs;
+
+        public MyKinect()
+        {
+            // one sensor is currently supported
+            this.kinectSensor = KinectSensor.GetDefault();
+
+            // get the coordinate mapper
+            this.coordinateMapper = this.kinectSensor.CoordinateMapper;
+
+            // get the depth (display) extents
+            FrameDescription frameDescription = this.kinectSensor.DepthFrameSource.FrameDescription;
+
+            // get size of joint space
+            //this.displayWidth = frameDescription.Width;
+            //this.displayHeight = frameDescription.Height;
+
+            // open the reader for the body frames
+            this.bodyFrameReader = this.kinectSensor.BodyFrameSource.OpenReader();
+
+            // set IsAvailableChanged event notifier
+            this.kinectSensor.IsAvailableChanged += this.Sensor_IsAvailableChanged;
+
+            // open the sensor
+            this.kinectSensor.Open();
+
+            if (this.bodyFrameReader != null)
+            {
+                this.bodyFrameReader.FrameArrived += this.Reader_FrameArrived;
+            }
+        }
+
+        /// <summary>
+        /// Handles the event which the sensor becomes unavailable (E.g. paused, closed, unplugged).
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Sensor_IsAvailableChanged(object sender, IsAvailableChangedEventArgs e)
+        {
+            // on failure, set the status text
+            //this.StatusText = this.kinectSensor.IsAvailable ? Properties.Resources.RunningStatusText
+            //                                              : Properties.Resources.SensorNotAvailableStatusText;
+        }
+
+        /// <summary>
+        /// Handles the body frame data arriving from the sensor
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Reader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            bool dataReceived = false;
+
+            using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
+            {
+                if (bodyFrame != null)
+                {
+                    if (this.bodies == null)
+                    {
+                        this.bodies = new Body[bodyFrame.BodyCount];
+                    }
+
+                    // The first time GetAndRefreshBodyData is called, Kinect will allocate each Body in the array.
+                    // As long as those body objects are not disposed and not set to null in the array,
+                    // those body objects will be re-used.
+                    bodyFrame.GetAndRefreshBodyData(this.bodies);
+                    dataReceived = true;
+                }
+            }
+
+            if (dataReceived)
+            {
+                KinectIsReady = true;
+
+                foreach (Body body in this.bodies)
+                {
+                    if (body.IsTracked)
+                    {
+
+                        IReadOnlyDictionary<JointType, Joint> joints = body.Joints;
+
+                        // convert the joint points to depth (display) space
+                        Dictionary<JointType, Point> jointPoints = new Dictionary<JointType, Point>();
+
+                        foreach (JointType jointType in joints.Keys)
+                        {
+                            // sometimes the depth(Z) of an inferred joint may show as negative
+                            // clamp down to 0.1f to prevent coordinatemapper from returning (-Infinity, -Infinity)
+
+                            CameraSpacePoint position = joints[jointType].Position;
+                            //CameraSpacePoint position = mockupJoints[i][jointType].Position;
+
+                            if (position.Z < 0)
+                            {
+                                position.Z = InferredZPositionClamp;
+                            }
+
+                            DepthSpacePoint depthSpacePoint = this.coordinateMapper.MapCameraPointToDepthSpace(position);
+                            jointPoints[jointType] = new Point(depthSpacePoint.X, depthSpacePoint.Y);
+                        }
+
+                        // Copy data to public attributes  
+                        Joints = joints.ToDictionary(k => k.Key, v => v.Value);
+                        JointPoints = jointPoints;
+                    }
+                    if (record && Joints != null && JointPoints != null)
+                    {
+                        int i = 0, max = Enum.GetValues(typeof(JointType)).Length;
+                        foreach (JointType jointType in Enum.GetValues(typeof(JointType)))
+                        {
+                            //Debug.WriteLine(JointPoints[jointType].X + " " + JointPoints[jointType].Y);
+                            if (i < max - 1)
+                            {
+                                AddText(fs, Joints[jointType].Position.X + " " + Joints[jointType].Position.Y + " " + Joints[jointType].Position.Z + " " + JointPoints[jointType].X + " " + JointPoints[jointType].Y + " ");
+                            }
+                            else
+                            {
+                                AddText(fs, Joints[jointType].Position.X + " " + Joints[jointType].Position.Y + " " + Joints[jointType].Position.Z + " " + JointPoints[jointType].X + " " + JointPoints[jointType].Y);
+                            }
+
+                            i++;
+                        }
+                        AddText(fs, "\n");
+                    }
+                }
+
+            }
+        }
+
+        public void StartRecording()
+        {
+            if (File.Exists(path))
+            {
+                //File.Delete(path);
+            }
+
+            fs = File.Create(path);
+            record = true;
+        }
+
+        public void StopRecording()
+        {
+            record = false;
+            fs.Close();
+        }
+
+        public bool IsRecording()
+        {
+            return record;
+        }
+
+        private static void AddText(FileStream fs, string value)
+        {
+            if (value == "\n")
+            {
+                byte[] newline = Encoding.ASCII.GetBytes(Environment.NewLine);
+                fs.Write(newline, 0, newline.Length);
+            }
+            else
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes(value);
+                fs.Write(info, 0, info.Length);
+                //byte[] space = Encoding.ASCII.GetBytes(" ");
+                //fs.Write(space, 0, space.Length);
+            }
+        }
+
+
+    }
+}
